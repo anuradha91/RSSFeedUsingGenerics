842b591a182402a20e9f5d17b2f0fa27839a5796
Anuradha Sharma <anuradha@anuradhas-macbook-pro.local>
2019-01-14 14:55:00 -0500

    Initial Commit

---
+++ WSJFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSJFeed
+//
+//  Created by Anuradha Sharma on 1/14/19.
+//  Copyright © 2019 Anuradha Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WSJFeedTests/WSJFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  WSJFeedTests.swift
+//  WSJFeedTests
+//
+//  Created by Anuradha Sharma on 1/14/19.
+//  Copyright © 2019 Anuradha Sharma. All rights reserved.
+//
+
+import XCTest
+@testable import WSJFeed
+
+class WSJFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
